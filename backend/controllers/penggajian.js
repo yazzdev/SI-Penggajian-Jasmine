fd8753021b60293e56260ca0e8a562276a30877c
Dyaz Amrullah <dyaz.amrullah@gmail.com>
2024-01-25 14:12:06 +0700

    [Backend] Add show detail penggajian

--- backend/controllers/penggajian.js
+++ backend/controllers/penggajian.js
@@ -22,20 +22,6 @@ module.exports = {
                   exclude: ['id','createdAt', 'updatedAt']
                 },
               },
-              {
-                model: Tunjangan,
-                as: 'tunjangan',
-                attributes: {
-                  exclude: ['id','nip_pegawai','createdAt', 'updatedAt']
-                },
-              },
-              {
-                model: Potongan,
-                as: 'potongan',
-                attributes: {
-                  exclude: ['id', 'nip_pegawai', 'createdAt', 'updatedAt']
-                },
-              }
             ]
           }
         ]
@@ -54,5 +40,70 @@ module.exports = {
         data: null,
       });
     }
-  }
+  },
+  showDetail: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const penggajian = await Penggajian.findOne({
+        where: { id },
+        attributes: {
+          exclude: ['createdAt', 'updatedAt']
+        },
+        include: [
+          {
+            model: Pegawai,
+            as: 'pegawai',
+            attributes: {
+              exclude: ['id', 'nip', 'id_jabatan', 'createdAt', 'updatedAt']
+            },
+            include: [
+              {
+                model: Jabatan,
+                as: 'jabatan',
+                attributes: {
+                  exclude: ['id', 'createdAt', 'updatedAt']
+                },
+              },
+              {
+                model: Tunjangan,
+                as: 'tunjangan',
+                attributes: {
+                  exclude: ['id', 'nip_pegawai', 'createdAt', 'updatedAt']
+                },
+              },
+              {
+                model: Potongan,
+                as: 'potongan',
+                attributes: {
+                  exclude: ['id', 'nip_pegawai', 'createdAt', 'updatedAt']
+                },
+              }
+            ]
+          }
+        ]
+      });
+
+      if (!penggajian) {
+        return res.status(404).json({
+          status: false,
+          message: 'Penggajian tidak ditemukan!',
+          data: null
+        });
+      }
+
+      return res.status(200).json({
+        status: true,
+        message: 'Success',
+        data: penggajian
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        status: false,
+        message: 'Internal Server Error',
+        data: null,
+      });
+    }
+  },
 };
--- backend/routes/penggajian.js
+++ backend/routes/penggajian.js
@@ -4,5 +4,6 @@ const { penggajian } = require('../controllers');
 const middlewares = require('../utils/middlewares');
 
 router.get('/show', middlewares.auth, penggajian.show);
+router.get('/show/:id', middlewares.auth, penggajian.showDetail);
 
 module.exports = router;
